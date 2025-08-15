c2c7672d899cf208e8df1916479086580e5c609f
nidhi01bhagat <nidhi01bhagat@gmail.com>
2025-08-15 12:05:35 +0530

    chapter 2

--- Chapter1/first.py
+++ Chapter1/first.py
@@ -1,7 +1,18 @@
 print("Hello, World!")
 print("*" * 10)
 
-#linting code
-
+# #linting code
 print "Hello World"
+2 + 3
 
+# PYTHON ENHANCEMENT PROPOSAL (formatting)
+x = 1
+y = 2
+unit_price = 3
+
+print("Hello World!")
+
+#Python implementations 
+# jython - java
+# ironpython - c
+# pypy - subset of python 
---
+++ chapter2/fundamentals.py
@@ -0,0 +1,4 @@
+students_count = 1000 
+rating = 4.5 #float
+is_published = True #boolean
+print(students_count)
